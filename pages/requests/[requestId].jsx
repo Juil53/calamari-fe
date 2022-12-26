7ffd90ba6a8c39549f2e1b1d71cb9c9d3ef0e31e
Nghia Dinh <nghiadvh@pivot.vn>
2022-12-26 13:21:43 +0700

    fix path request detail page

--- pages/requests/[requestId].jsx
+++ pages/requests/[requestId].jsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Loading from "../../../components/Loading";
+import Loading from "../../components/Loading";
 
 const RequestDetail = ({ request }) => {
   const router = useRouter();
